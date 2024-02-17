619f673f097b2d56ed916be67dbecf0c16f615a6
Lizard <josephmhenry42@gmail.com>
2024-02-16 23:04:09 -0600

    fix index.html location

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ app.use(express.static("public"));
 app.use("/api", api);
 
 // This view route is a GET route for the homepage
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
