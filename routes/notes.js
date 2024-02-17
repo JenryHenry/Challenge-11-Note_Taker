8a05ec9d80aa355e3e706c543fcf6e2264af2767
Lizard <josephmhenry42@gmail.com>
2024-02-16 22:16:27 -0600

    add id to note data

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,37 @@
 const router = require("express").Router();
 const notesData = require("../db/db.json");
+const fs = require("fs");
 
 router.get("/", (req, res) => {
-  res.json(notesData);
+  const displayNotes = notesData.map((item, idx) => {
+    return {
+      id: idx,
+      title: item.title,
+      text: item.text,
+    };
+  });
+  res.json(displayNotes);
 });
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const { title, text } = req.body;
+  //   console.log(req.body);
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+    console.log(newNote);
+    notesData.push(newNote);
+    const noteString = JSON.stringify(notesData, null);
+    fs.writeFile("./db/db.json", noteString);
+  }
+  res.json(req.body);
+});
 
-router.delete("/:index", (req, res) => {});
+router.delete("/:title", (req, res) => {
+  notesData.splice();
+});
 
 module.exports = router;
